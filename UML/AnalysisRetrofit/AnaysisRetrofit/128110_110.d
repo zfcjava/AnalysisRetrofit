318b096dd96759d5a361aa0caba203c60774a982
zfc <zhangfengcheng@hongtao-edu.com>
2018-05-23 17:33:16 +0800

    增加uml

---
+++ UML/AnalysisRetrofit/AnaysisRetrofit/128110_110.d
@@ -0,0 +1,35 @@
+format 76
+
+classcanvas 128110 class_ref 128494 // Retrofit
+  draw_all_relations default hide_attributes default hide_operations default hide_getset_operations default show_members_full_definition default show_members_visibility default show_members_stereotype default show_members_context default show_members_multiplicity default show_members_initialization default show_attribute_modifiers default member_max_width 0 show_parameter_dir default show_parameter_name default package_name_in_tab default class_drawing_mode default drawing_language default show_context_mode default auto_label_position default show_relation_modifiers default show_relation_visibility default show_infonote default shadow default show_stereotype_properties default
+  xyz 27.4 105.4 2000
+end
+classcanvas 128238 class_ref 130798 // ServiceMethod
+  draw_all_relations default hide_attributes default hide_operations default hide_getset_operations default show_members_full_definition default show_members_visibility default show_members_stereotype default show_members_context default show_members_multiplicity default show_members_initialization default show_attribute_modifiers default member_max_width 0 show_parameter_dir default show_parameter_name default package_name_in_tab default class_drawing_mode default drawing_language default show_context_mode default auto_label_position default show_relation_modifiers default show_relation_visibility default show_infonote default shadow default show_stereotype_properties default
+  xyz 69.1 756.9 2006
+end
+classcanvas 128494 class_ref 130670 // OkHttpCall
+  draw_all_relations default hide_attributes default hide_operations default hide_getset_operations default show_members_full_definition default show_members_visibility default show_members_stereotype default show_members_context default show_members_multiplicity default show_members_initialization default show_attribute_modifiers default member_max_width 0 show_parameter_dir default show_parameter_name default package_name_in_tab default class_drawing_mode default drawing_language default show_context_mode default auto_label_position default show_relation_modifiers default show_relation_visibility default show_infonote default shadow default show_stereotype_properties default
+  xyz 1132.3 164.8 2012
+end
+classcanvas 128750 class_ref 129774 // CallAdapter
+  draw_all_relations default hide_attributes default hide_operations default hide_getset_operations default show_members_full_definition default show_members_visibility default show_members_stereotype default show_members_context default show_members_multiplicity default show_members_initialization default show_attribute_modifiers default member_max_width 0 show_parameter_dir default show_parameter_name default package_name_in_tab default class_drawing_mode default drawing_language default show_context_mode default auto_label_position default show_relation_modifiers default show_relation_visibility default show_infonote default shadow default show_stereotype_properties default
+  xyz 543.1 24.6 2012
+end
+relationcanvas 128366 relation_ref 134894 // <unidirectional association>
+  from ref 128110 z 2007 stereotype "<<Map>>" xyz 178.5 606 2007 to ref 128238
+  role_a_pos 141 736 3000 no_role_b
+  no_multiplicity_a no_multiplicity_b
+end
+relationcanvas 128622 relation_ref 130670 // <unidirectional association>
+  from ref 128494 z 2013 to point 668 501.7
+  line 129006 z 2013 to ref 128238
+  role_a_pos 220 843 3000 no_role_b
+  no_multiplicity_a no_multiplicity_b
+end
+relationcanvas 128878 relation_ref 135662 // <unidirectional association>
+  from ref 128238 z 2013 to ref 128750
+  role_a_pos 568 130 3000 no_role_b
+  no_multiplicity_a no_multiplicity_b
+end
+end
